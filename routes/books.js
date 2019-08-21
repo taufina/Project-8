570a521e5b55cea0aa72589437884b101792d9f9
Nabila Taufiq <nabila.taufiq@verizon.com>
2019-08-21 17:28:43 -0500

    Worked in errors

--- routes/books.js
+++ routes/books.js
@@ -6,9 +6,10 @@ const Book = require("../models").Book; //this lets us use book model and all th
 router.get('/', function(req, res, next) {
   Book.findAll({order: [["title", "ASC"]]})
   .then(function(books){
-    res.render("index", {books: books, title: "My Awesome Library" });
+    res.render("index", {books: books, title: "Nabila's Library" });
   }).catch(function(err){
     res.send(500, err);
+    res.render('errors', {err})
   });
 });
 
@@ -19,15 +20,16 @@ router.get('/new', function(req, res, next) {
 });
 
 /* POST create book. */
-router.post('/books/new', function(req, res, next) {
+router.post('/new', function(req, res, next) {
+  let {title, author, genre, year} = req.body;
   Book.create(req.body).then(function(book){
     res.redirect("/books/" + book.id);
   }).catch(function(err){
     if(err.name === "SequelizeValidationError"){
       //render
-      res.render("new-book", {
-        book: Book.build(req.body), 
-        title: "Enter a New Book",
+      res.render("show", {
+        // book: Book.build(req.body), 
+        // title: "Enter a New Book",
         errors: err.errors
       });
     } else {
@@ -35,6 +37,7 @@ router.post('/books/new', function(req, res, next) {
     }
   }).catch(function(err){
     res.send(500, err);
+    res.render('errors', {err})
   });
   //books.push(book);
 });
@@ -49,17 +52,16 @@ router.get('/:id/delete', function (req, res, next) {
     }
   }).catch(function(err){
     res.send(500);
+    res.render('errors', {err})
   });
 });
-
-
 
 
 
 // /* PUT update book. */
 
 // //updating a method is returning a promise passes the next value down the then chain.
-router.put('/:id', function (req, res, next) {
+router.post('/:id', function (req, res, next) {
   Book.findByPk(req.params.id).then((book) => {
     if(book){
       return book.update(req.body);
@@ -75,7 +77,7 @@ router.put('/:id', function (req, res, next) {
       book.id = req.params.id;
 
       res.render("update-book", {
-        book: book, 
+        book: req.body, 
         title: "Update Book",
         errors: err.errors
       });
@@ -84,46 +86,43 @@ router.put('/:id', function (req, res, next) {
     }
   }).catch(function(err){
     res.send(500);
+    res.render('errors', {err})
+
   });
 });
 
 // /* DELETE individual book. */
-router.delete('/:id/delete', function (req, res, next) {
+router.post('/:id/delete', function (req, res, next) {
   Book.findByPk(req.params.id).then((book) => {
     if(book){
       return book.destroy();  //destroy method returns a promise, once the promsie is fulfilled, then we redirect to the books path
     }else {
       res.send(404);
+      res.render('errors', {err})
+
     }
   }).then(() => {
     res.redirect('/books');
   }).catch(function(err){
     res.send(500);
+    res.render('errors', {err})
+
   });
 });
+
 
 // /* GET individual book. */
 router.get('/:id', function(req, res, next) {
   Book.findByPk(req.params.id).then((book) => {
     if(book){
-      try{
-        res.locals.book = book;
-        res.locals.title = book.title;
-        res.render('update-book');
-      }catch (error) {
-        console.error(error);
-      }
+      res.render('show', {book});
     } else {
       res.send(404);
     }
   }).catch(function(err){
+    res.render('errors', {err})
     res.send(500);
   });
-});
-router.get("/:id", function(req, res, next){
-  const book = find(req.params.id);
-
-  res.render("books/show", {book: book, title: book.title});
 });
 
 
@@ -133,10 +132,11 @@ router.get('/:id/edit', function (req, res, next) {
     if(book) {
       res.render('update-book', { book: book, title: 'Edit Book' });
     } else {
-      res.send(404);
+      res.render('page-not-found', {err})
     }
   }).catch(function(err){
     res.send(500);
+    res.render('errors', {err})
   });
 });
 
